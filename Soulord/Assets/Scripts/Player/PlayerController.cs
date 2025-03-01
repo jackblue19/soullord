7560d6511dd2f939ab77f315ae7b07037b61020f
Jack Blue <git.jack1624@gmail.com>
2025-03-01 15:06:45 +0700

    movement + physics

--- Soulord/Assets/Scripts/Player/PlayerController.cs
+++ Soulord/Assets/Scripts/Player/PlayerController.cs
@@ -3,15 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1.0f;    
+    [SerializeField] private float moveSpeed = 1.0f;
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void OnEnable()
     {
@@ -29,15 +33,34 @@ public class PlayerController : MonoBehaviour
         {
             movement = movement.normalized;
         }
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX" , movement.x);
+        myAnimator.SetFloat("moveY" , movement.y);
     }
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if ( mousePos.x < playerScreenPoint.x )
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
     }
 }
